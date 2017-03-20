2cd10deea44d7ba31177d91c0782dad9c4b29126
Mathias Wagner <mathiasw@nvidia.com>
2017-03-20 16:07:25 +0100

    configure a header file that includes compile time definitions necessary to make quda behave properly when used through C++ interface (not yet complete, this is rather a proof of concept)

--- include/qudacxx.h
+++ include/qudacxx.h
@@ -2,17 +2,18 @@
 #define _QUDACXX_H
 
 /**
- * @file  quda.h
- * @brief Main header file for the QUDA library
+ * @file  qudacxx.h
+ * @brief C++ header file for the QUDA library
  *
- * Note to QUDA developers: When adding new members to QudaGaugeParam
- * and QudaInvertParam, be sure to update lib/check_params.h as well
- * as the Fortran interface in lib/quda_fortran.F90.
  */
 
+#include <qudaconfigure.h>
 #include <enum_quda.h>
 #include <stdio.h> /* for FILE */
 #include <quda_constants.h>
+// include support for QUDA gauge_field, color_spinor_field and blas operations 
+#include <gauge_field.h>
+#include <blas_quda.h>
 
 void computeKSLinkQuda(cudaGaugeField* cudaFatLink, cudaGaugeField* cudaLongLink, cudaGaugeField* cudaUnitarizedLink, cudaGaugeField* cudaInLink, double *path_coeff, QudaGaugeParam *param) {
 
